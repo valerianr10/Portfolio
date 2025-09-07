51e5b85669c5116e788dc8359b32a49eacc9e00e
valerianr10 <valerianrvd11@gmail.com>
2025-09-07 17:33:36 +0700

    fixing 2.0

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,15 +4,15 @@ const Footer = () => {
         <h1 className="text-3xl font-mono font-bold text-rose-100">Find me on</h1>
         <div className="text-lg font-mono font-bold flex gap-7">
             <a href="#home">Top <i class="ri-arrow-up-long-line"></i></a>
-        </div>
+        </div >
         <div className="flex items-center gap-3">
-            <a href="https://github.com/valerianr10">
+            <a target="_blank" href="https://github.com/valerianr10">
                 <i className="ri-github-fill ri-3x"></i>
             </a>
-            <a href="https://www.linkedin.com/in/valerian-rivaldi-7b2ab5228/">
+            <a target="_blank" href="https://www.linkedin.com/in/valerian-rivaldi-7b2ab5228/">
                 <i className="ri-linkedin-fill ri-3x"></i>
             </a>
-            <a href="https://www.instagram.com/valerianrvd/">
+            <a target="_blank" href="https://www.instagram.com/valerianrvd/ ">
                 <i className="ri-instagram-fill ri-3x"></i>
             </a>
         </div>
